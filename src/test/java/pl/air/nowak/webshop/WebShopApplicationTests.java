d483ca14c3b160344d63f4cb0526381a5ab8b32e
Sebastian Nowak <nowaksebastian998@gmail.com>
2023-03-16 15:46:53 +0100

    Stworzenie i dodanie aplikacji na gita

---
+++ src/test/java/pl/air/nowak/webshop/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.air.nowak.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
