655cc4ba55e49626044e4e5528830750772aeba0
Sebastian Nowak <nowaksebastian998@gmail.com>
2023-03-29 00:09:58 +0200

    Wprowadzenie obsługi koszyka

--- src/main/java/pl/air/nowak/webshop/Model/Item.java
+++ src/main/java/pl/air/nowak/webshop/Model/Item.java
@@ -21,3 +21,13 @@ public class Item {
     private BigDecimal price;
     private String imgUrl;
 }
+
+/*
+    static{
+            items.add(new Item(1,"Halloween Ends 4K UHD + Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/product_thumb.php?img=images/products/halloween%20ends%204k%20uhd.jpg&w=278&h=400"));
+            items.add( new Item(2,"Puss in Boots: The Last Wish Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/images/products/pussinboots_thelastwish-P-BD-k.png"));
+            items.add( new Item(3,"Captain Marvel 3D + 2D Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/images/products/captain%20marvel%203d%20blu%20ray.jpg"));
+            items.add( new Item(4,"The Last of Us: The Complete First Season Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/images/products/the%20last%20of%20us%20blu%20ray.jpg"));
+            items.add( new Item(5,"Rocky: The Knockout Collection 4K UHD",new BigDecimal("100"),"https://www.futuremovieshop.fi/images/products/rocky%20knockout%20collection%204k.jpg"));
+    }
+*/
--- src/main/java/pl/air/nowak/webshop/controller/HomeController.java
+++ src/main/java/pl/air/nowak/webshop/controller/HomeController.java
@@ -1,6 +1,7 @@
 package pl.air.nowak.webshop.controller;
 
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +12,10 @@ import pl.air.nowak.webshop.Repository.MovieRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
-
-
-    protected static List<Item> items = new ArrayList<>();
 
     @Autowired
     MovieRepository movieRepository;
@@ -26,24 +25,28 @@ public class HomeController {
     }
 
 
-    public List<Item> getAll() {
+    private List<Item> getAll() {
         return movieRepository.getAll();
     }
-
-/*
-    static{
-            items.add(new Item(1,"Halloween Ends 4K UHD + Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/product_thumb.php?img=images/products/halloween%20ends%204k%20uhd.jpg&w=278&h=400"));
-            items.add( new Item(2,"Puss in Boots: The Last Wish Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/images/products/pussinboots_thelastwish-P-BD-k.png"));
-            items.add( new Item(3,"Captain Marvel 3D + 2D Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/images/products/captain%20marvel%203d%20blu%20ray.jpg"));
-            items.add( new Item(4,"The Last of Us: The Complete First Season Blu Ray",new BigDecimal("80.10"),"https://www.futuremovieshop.fi/images/products/the%20last%20of%20us%20blu%20ray.jpg"));
-            items.add( new Item(5,"Rocky: The Knockout Collection 4K UHD",new BigDecimal("100"),"https://www.futuremovieshop.fi/images/products/rocky%20knockout%20collection%204k.jpg"));
-    }
-*/
-
 
 
     @GetMapping("/")
     public String home(Model model){
+        model.addAttribute("items", getAll());
+        return "home";
+    }
+
+    @GetMapping("/add/{idmovies2}")
+    public String addItemToCart(@PathVariable("idmovies2") int idmovies2, Model model, HttpSession session){
+        @SuppressWarnings("unchecked")
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        if(cart == null){
+            cart = new ArrayList<>();
+        }
+        Item oItem = movieRepository.getById(idmovies2);
+            cart.add(oItem);
+            session.setAttribute("cart", cart);
+
         model.addAttribute("items", getAll());
         return "home";
     }
